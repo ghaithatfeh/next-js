9e40dca760d5b85e7e33f483620f265c027f3fef
Ghaith Atfeh <ghaith.atfeh@l-one.de>
2024-08-22 14:43:28 +0300

    Routing and Params

---
+++ app/component/Button.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Button = () => {
+	return (
+		<div>
+			<button className="btn btn-primary" onClick={() => console.log("asdas")}>asdd</button>
+		</div>
+	);
+};
+
+export default Button;
---
+++ app/users/[id]/photos/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PhotosPage = async () => {
+	return (
+		<>
+			<h1>PhotosPage</h1>
+		</>
+	);
+};
+
+export default PhotosPage;
